277615910b9c04ac30818e023d4228bfb4456e19
Song Minjin <m_6595@naver.com>
2023-03-08 17:57:44 +0900

    #25 feat(caching) : 캐싱 관련 설정

--- src/main/java/com/demo/coffeePosMachine/beverage/PopularBeverage.java
+++ src/main/java/com/demo/coffeePosMachine/beverage/PopularBeverage.java
@@ -1,4 +1,24 @@
 package com.demo.coffeePosMachine.beverage;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.Table;
+
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "popularBeverage")
 public class PopularBeverage {
+    Long beverageId;
+
+    String beverageName;
+
+    int beveragePrice;
+
+    Long count;
 }
